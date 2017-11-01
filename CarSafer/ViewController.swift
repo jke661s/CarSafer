1000344c23a30be17cdbe5687975e49c1af01431
Jackie Wang <jke661s@gmail.com>
2017-11-01 11:58:34 +1100

    Initial Commit

---
+++ CarSafer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarSafer
+//
+//  Created by Jiaqi Wang on 1/11/17.
+//  Copyright © 2017 Jiaqi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
